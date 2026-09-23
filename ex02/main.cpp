03c9c279086d3ab9059cb6956762be360998b757
yuczhang <yuczhang@f2r11s13.paris.42.school>
2026-09-23 17:29:50 +0200

    sdf

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
@@ -25,5 +26,30 @@ int main()
 		std::cout << *it << std::endl;
 	}
 
+	std::cout << "\n==========  STD::LIST ==========" << std::endl;
+    std::list<int> mlist;
+
+    // 栈的 push 对应 list 的 push_back
+    mlist.push_back(5);
+    mlist.push_back(17);
+
+    // 栈的 top 对应 list 的 back (获取尾部元素)
+    std::cout << "Top element: " << mlist.back() << std::endl;
+
+    // 栈的 pop 对应 list 的 pop_back (移除尾部元素)
+    mlist.pop_back();
+
+    std::cout << "Size after pop: " << mlist.size() << std::endl;
+
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    mlist.push_back(0);
+
+    std::cout << "Elements in List:"<< std::endl;
+    for (std::list<int>::iterator lit = mlist.begin(); lit != mlist.end(); ++lit)
+    {
+        std::cout << *lit << std::endl;
+    }
 	return 0;
 }
